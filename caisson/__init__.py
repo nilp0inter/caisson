1dab47f72eec297dc92622c7c6df52713a4ae67c
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-08-03 12:25:25 +0200

    Only keeping __version__ at __init__.

--- caisson/__init__.py
+++ caisson/__init__.py
@@ -1,11 +1,5 @@
 import logging
 
 __version__ = "0.0.3"
-__license__ = "GPL2"
-__description__ = "Recursively decompress files"
-__keywords__ = "compress, decompress, unzip, 7z, unrar"
-__author__ = "Roberto Abdelkader Martínez Pérez"
-__author_email__ = "robertomartinezp@gmail.com"
-__url__ = "https://github.com/nilp0inter/caisson"
 
 log = logging.getLogger(__name__)
